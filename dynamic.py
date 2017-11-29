a42f4baa95659da801f8adfb2705a5585e51f583
ruchika <ruchikashete2411@gmail.com>
2017-11-29 17:51:02 +0530

    second version

--- dynamic.py
+++ dynamic.py
@@ -1,3 +1,39 @@
+#import Adafruit_BBIO.GPIO as g
+import sys
+import time as t
+dp=0
+max1=5
+
+def upward(dp,tp):
+   for i in range(tp+1):
+      pin="P9_"+str(11+dp+i)
+      print "Floor No: "+str(dp+i)
+      #g.setup(pin,g.OUT)
+      #g.output(pin,g.HIGH)
+      t.sleep(1)
+      #g.setup(pin,g.OUT)
+      #g.output(pin,g.LOW)
+   #g.setup(pin,g.OUT)
+   #g.output(pin,g.HIGH)
+   print "Reached To Floor"
+   t.sleep(3)
+   #g.setup(pin,g.OUT)
+   #g.output(pin,g.LOW)
+def downword(dp,tp):
+   for i in range(tp+1):
+      pin="P9_"+str(11+dp-i)
+      #g.setup(pin,g.OUT)
+      #g.output(pin,g.HIGH)
+      print "Floor No: "+str(dp-i)
+      t.sleep(1)
+      #g.setup(pin,g.OUT)
+      #g.output(pin,g.LOW)
+   #g.setup(pin,g.OUT)
+   #g.output(pin,g.HIGH)
+   print "Reached To Floor"
+   t.sleep(3)
+   #g.setup(pin,g.OUT)
+#g.output(pin,g.LOW)
 
 while 1:
   choice=map(int,raw_input("Enter your choice:"))
